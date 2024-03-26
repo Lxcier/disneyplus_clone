766c5594ae87b30319807a6c4f7c63b8d77c8aa1
Lucas Lucier <lucasbr.gto@gmail.com>
2024-03-25 21:25:55 -0300

    ajustes no mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else (
+            exibeElementosDoHeader()
+        )
+    })
+
+
+    // Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener ('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -12,7 +28,29 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
-})
+
+
+    // seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+
+function ocultaElementosDoHeader (){
+    const header = document.querySelector('header');
+    header.classList.add ('header--is-hidden')
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove ('header--is-hidden')
+}
+
+function abreOuFechaResposta (elemento){
+    const classe ='faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -29,3 +67,4 @@ function escondeTodasAbas() {
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
 }
+})
